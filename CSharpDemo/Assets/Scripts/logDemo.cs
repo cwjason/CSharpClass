a87c6735f379cc7ea0da64438a4b023d4d68bb3f
Qivi <875593702@qq.com>
2019-02-20 00:19:32 +0800

    提交最新文件修改

---
+++ CSharpDemo/Assets/Scripts/logDemo.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class logDemo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("1111");
+
+        Debug.Log("1111");
+        Debug.LogWarning("1111");
+        Debug.LogError("1111");
+
+        //快捷注释ctl+k,ctl+c ,解开注释ctl+k, ctl + us
+
+        //变量
+        int hp = 100;
+        int sp = 200;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
